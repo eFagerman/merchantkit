43e6ceeedcfabdbba0009c7b25888b64d0079562
eFagerman <e.fagerman@gmail.com>
2021-02-27 20:18:50 +0100

    Update package description

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MerchantKit",
+    platforms: [
+        .iOS(.v10), .macOS(.v11)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -20,9 +23,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MerchantKit",
-            dependencies: []),
+            dependencies: [],
+            path: "Source"),
         .testTarget(
             name: "MerchantKitTests",
-            dependencies: ["MerchantKit"]),
+            dependencies: ["MerchantKit"],
+            path: "Tests")
     ]
 )
